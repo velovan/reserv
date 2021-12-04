663f171aeb4fac5a8042d6786b4cf56297009773
velovan <velovan@users.noreply.github.com>
2021-12-04 15:55:54 +0200

    Update HotelException.java

--- Reservation/src/reservationSystem/HotelException.java
+++ Reservation/src/reservationSystem/HotelException.java
@@ -2,8 +2,8 @@ package reservationSystem;
 
 @SuppressWarnings("serial")
 public class HotelException extends RuntimeException {
+	
 	public HotelException(String msg) {
-		super(msg);
+	  super(msg);
 	}
-
 }
